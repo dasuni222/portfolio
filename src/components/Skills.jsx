e4cc604c56685dc8bac0b485e4196bdd8f5edbe0
Dasuni Navodya <dasunirajapaksha2002@gmail.com>
2026-06-04 14:37:46 +0530

    Updated my protfolio

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,17 +3,17 @@ function Skills() {
     <section>
       <h2>Skills</h2>
 
-      <ul>
-        <li>Java</li>
-        <li>Python</li>
-        <li>React.js</li>
-        <li>Next.js</li>
-        <li>Node.js</li>
-        <li>JavaScript</li>
-        <li>MySQL</li>
-        <li>Firebase</li>
-        <li>Git & GitHub</li>
-      </ul>
+      <div className="skills-list">
+        <span>Java</span>
+        <span>Python</span>
+        <span>React.js</span>
+        <span>Next.js</span>
+        <span>Node.js</span>
+        <span>JavaScript</span>
+        <span>MySQL</span>
+        <span>Firebase</span>
+        <span>Git & GitHub</span>
+      </div>
     </section>
   );
 }
